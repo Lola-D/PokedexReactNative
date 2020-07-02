4506eeaba71ad8d74704c8e797d5b1a276b50017
Lola <lola.donval@gmail.com>
2020-07-02 16:43:07 +0200

    create application Pokedex

---
+++ src/components/Sac.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class Sac extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Sac</Text>
+      </View>
+    )
+  }
+}
+
+export default Sac
